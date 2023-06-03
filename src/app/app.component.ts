bca13e69a768c173706930aec7cbc986ed571949
Alberto Boyano Martín <boyano98@gmail.com>
2023-06-03 13:37:09 +0200

    Se renderizan las imágenes que se necesiten mostrar únicamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-app';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'changed';
+  }
+
+  ngOnInit() {
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+      });
+  }
 }
